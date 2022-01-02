648343bf1ab73b19ee7adbb8733bb94aa0a3c313
zyxjeek <yixun2010@outlook.com>
2022-01-02 18:22:44 +0800

    commit git

---
+++ scoreboard.py
@@ -0,0 +1,97 @@
+import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
+
+
+class Scoreboard:
+    """A class to report scoring information."""
+
+    def __init__(self, ai_settings, screen, stats):
+        """Initialize scorekeeping attributes."""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
+        self.stats = stats
+
+        # Font settings for scoring information.
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont('华文仿宋', 32)
+
+        # Prepare the initial score images.
+        self.prep_image()
+
+    def prep_image(self):
+        self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
+
+    def prep_score(self):
+        """Turn the score into a rendered image."""
+        self.get_score_image()
+
+        # Display the score at the top right of the screen.
+        self.display_score()
+
+    def get_score_image(self):
+        rounded_score = int(round(self.stats.score, -1))
+        score_str = "得分：{:,}".format(rounded_score)
+        self.score_image = self.font.render(score_str, True, self.text_color,
+                                            self.ai_settings.bg_color)
+
+    def display_score(self):
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        self.get_high_score_image()
+
+        # Center the high score at the top of the screen.
+        self.display_high_score()
+
+    def display_high_score(self):
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def get_high_score_image(self):
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "最高得分：{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True,
+                                                 self.text_color, self.ai_settings.bg_color)
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        self.level_image = self.font.render('等级:' + str(self.stats.level), True,
+                                            self.text_color, self.ai_settings.bg_color)
+
+        # Position the level below the score.
+        self.display_level()
+
+    def display_level(self):
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            self.draw_ship(ship_number)
+
+    def draw_ship(self, ship_number):
+        ship = Ship(self.ai_settings, self.screen)
+        ship.rect.x = 10 + ship_number * ship.rect.width
+        ship.rect.y = 10
+        self.ships.add(ship)
+
+    def show_all_score(self):
+        """Draw score to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        # Draw ships.
+        self.ships.draw(self.screen)
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=['alien.py', 'alien_invasion.py', 'bullet.py', 'button.py', 'game_functions.py',
+               'game_stats.py', 'scoreboard.py', 'settings.py', 'ship.py'])
